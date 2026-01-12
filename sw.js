48aff76b3d963edd04fe4765c9947ae3ae96e6aa
dexter-linux <152237140+dexter-linux@users.noreply.github.com>
2026-01-12 18:58:43 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = 'kifaru-frenzy-v1';
+const assetsToCache = [
+  './',
+  './index.html',
+  './style.css',
+  './sketch.js',
+  './manifest.json',
+  './song.mp3',
+  './effect.mp3'
+];
+
+// Install Event
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(assetsToCache);
+    })
+  );
+});
+
+// Fetch Event (Offline support)
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+// Activate Event
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      );
+    })
+  );
+});
